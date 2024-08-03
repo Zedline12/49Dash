71eb27d179da3cbbf546709559ee03158e5377cf
Zedline12 <bedomohamed307@gmail.com>
2024-08-03 17:36:54 +0300

    serverurl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://49dev.com/api/v1',
+  serverUrl: 'https://49dev.com/api/v1',
   accessToken: 'ACCESS_TOKEN',
   refreshToken: 'REFRESH_TOKEN',
 };
