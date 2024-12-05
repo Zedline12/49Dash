30a8f4137be3218c920d3f666d5aecd2c73b7a9b
Zedline12 <bedomohamed307@gmail.com>
2024-12-05 06:47:36 +0200

    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverUrl: 'http://192.168.1.11:3000',
+  serverUrl: 'https://benova.zeabur.app',
   accessToken: 'ACCESS_TOKEN',
   refreshToken: 'REFRESH_TOKEN',
 };
